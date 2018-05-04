940c011c6fd00212e7cf994c1ccc95463c080b21
funemployeddad <funemployeddad@gmail.com>
2018-05-03 21:16:41 -0700

    resteting vm

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -8,6 +8,14 @@ class DecksController < ApplicationController
     if !current_user
       redirect_to user_path(current_user)
     end
+
     @deck = Deck.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+  
 end
