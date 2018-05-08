e9f63f5057480e6017b00547bb6358aedb437246
funemployeddad <funemployeddad@gmail.com>
2018-05-07 20:14:08 -0700

    create deck works. create cards works. can switch decks and have the right one show. next steps are to get edit/update/destroy done

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -15,9 +15,24 @@ class DecksController < ApplicationController
   end
 
   def new
+    @deck = current_user.decks.build
+    @user = current_user
   end
 
   def create
+
+    @deck = current_user.decks.build(:name => params[:name])
+
+    if @deck.save
+      redirect_to user_deck_path(current_user, @deck)
+    end
+
+    ##need to put the redirect in for errors if the deck doesn't save ##
+  end
+
+  private
+  def deck_params
+    params.require(:decks).permit(:name, :user_id, :deck_id)
   end
 
 end
