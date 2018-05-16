177e5c1676ca1850e6e1ee3bd74b36efa3089c40
funemployeddad <funemployeddad@gmail.com>
2018-05-16 14:41:38 -0700

    trying to get errors to display. having issues in sessions#create getting it to have an @user for displaying the errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,27 +6,55 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # byebug
+
+
     if params[:name]
-      if User.find_by(:name => params[:name])
-         @user = User.find_by(:name => params[:name])
-       else
-         redirect_to 'sessions/new'
-       end
+      if params[:name].empty?
+        @user = User.new
+        render :new
+      else
+    	   @user = User.find_by(:name => params[:name])
+      end
     else
       @user = User.find_or_create_by(uid: auth['uid']) do |u|
         u.name = auth['info']['name']
         u.email = auth['info']['email']
         u.password = SecureRandom.hex(9)
-        end
+      end
     end
 
-    # if @user.valid?
-      @user.save
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    if @user.save
+    	session[:user_id] = @user.id
+    	redirect_to user_path(@user)
     # else
-    #   render :new
+    # 	render :new
+    end
+
+    # @user = User.new
+    # if params[:name].empty?
+    #  render :new
     # end
+
+    # if
+      # @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      #   u.name = auth['info']['name']
+      #   u.email = auth['info']['email']
+      #   u.password = SecureRandom.hex(9)
+      #   end
+    # else
+    #   params[:name] && !params[:name].empty?
+    #   if User.find_by(:name => params[:name])
+    #      @user = User.find_by(:name => params[:name])
+    #    else
+    #      redirect_to 'sessions/new'
+    #    end
+    #
+    # end
+
+      # session[:user_id] = @user.id
+      # redirect_to user_path(@user)
+
   end
 
   def destroy
