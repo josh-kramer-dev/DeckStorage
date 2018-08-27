68e4cd3e6e69b53e79b211d1498125c9e73dd183
funemployeddad <funemployeddad@gmail.com>
2018-08-27 15:45:22 -0700

    moving back to jquery branch

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -5,7 +5,7 @@ $(function(){
     $.get(this.href).success(function(res){ //get the href from the click event
       $("div.deck ul").html(''); //clear your target div
       $(".button_to").show(); //show the button. next step is to now add a form to the page for adding a card instead of a button that sends you to a new page.
-        res.forEach(function(card){ //iterate of the response data
+        res.forEach(function(card){ //iterate over the response data
           $("div.deck ul").append("<li>" + "<strong>" + card.name + "</strong>" + "<br>" + 'quantity: ' + card.quantity + "</li>"); //build the html string for the div
         });
       })
