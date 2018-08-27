a74163f072105dcfc2aa7cc6c53aaf9d25ec4468
funemployeddad <funemployeddad@gmail.com>
2018-08-27 15:46:40 -0700

    need to add form for new deck using ajax

---
+++ app/assets/javascripts/decks.js
@@ -0,0 +1,14 @@
+$(function(){
+  $(".button_to").hide();
+
+  $("a.load_deck").on('click', function(e){ //hijack the click event of selecting a deck
+    e.preventDefault(); //prevent the move to the new page
+    $.get(this.href).success(function(res){ //get the href from the click event
+      $("div.deck ul").html(''); //clear your target div
+      $(".button_to").show(); //show the button. next step is to now add a form to the page for adding a card instead of a button that sends you to a new page.
+        res.forEach(function(card){ //iterate over the response data
+          $("div.deck ul").append("<li>" + "<strong>" + card.name + "</strong>" + "<br>" + 'quantity: ' + card.quantity + "</li>"); //build the html string for the div
+        });
+      })
+    })
+  })
