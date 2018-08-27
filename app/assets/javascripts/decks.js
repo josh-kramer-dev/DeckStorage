7bf8cbee334fb422921eed7063ac9d099f0aa2e3
funemployeddad <funemployeddad@gmail.com>
2018-08-27 01:03:20 -0700

    deck is now being appended to the correct div, clearing between decks, and is ok formatting. need to add the ability to add cards to a deck on this show page.

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -1,25 +1,11 @@
 $(function(){
-  $("a.load_deck").on('click', function(e){
-    e.preventDefault();
-    $.get(this.href).success(function(){
-      debugger
-        var $card =  $.get("div.deck-show").html();
-        $("div.deck").html($card);
+  $("a.load_deck").on('click', function(e){ //hijack the click event of selecting a deck
+    e.preventDefault(); //prevent the move to the new page
+    $.get(this.href).success(function(res){ //get the href from the click event
+      $("div.deck ul").html(''); //clear your target div
+        res.forEach(function(card){ //iterate of the response data
+          $("div.deck ul").append("<li>" + "<strong>" + card.name + "</strong>" + "<br>" + '    quantity: ' + card.quantity + "</li>"); //build the html string for the div
+        });
       })
     })
   })
-
-
-
-//this works to display the name and quantities, but it's ugly
-  // $(function(){
-  //   $("a.load_deck").on('click', function(e){
-  //     e.preventDefault();
-  //     $.get(this.href).success(function(response){
-  //       response.forEach(function(card){
-  //         var $card =  "<li>" + card.name + " " + "    quantity: " + card.quantity + "</li>";
-  //         $("div.deck ul").append($card);
-  //         })
-  //       })
-  //     })
-  //   })
