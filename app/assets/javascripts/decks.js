eee74bc698f1446b70aac67462f79bf83d14665e
funemployeddad <funemployeddad@gmail.com>
2018-10-20 12:56:37 -0700

    adding deck id so I have a better target for toggle()

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -5,7 +5,6 @@ $(function(){
     e.preventDefault(); //prevent the move to the new page
     $.get(this.href).success(function(res){ //get the href from the click event
 
-      $("div.deck ul").html(""); //clear your target div
       $(".button_to").show(); //show the button. next step is to now add a form to the page for adding a card instead of a button that sends you to a new page.
       $(".table-cards").toggle(); //need to get it to only toggle the list for that particular deckjosh
       })
@@ -13,15 +12,8 @@ $(function(){
   })
 
 
-  // res.forEach(function(card){ //iterate over the response data
-  //   $("div.deck ul").append("<li>" + "<strong>" + card.name + "</strong>" + "<br>" + 'quantity: ' + card.quantity + "</li>"); //build the html string for the div
-  //   });
+//hijack the card delete button and use it to remove the card but stay on the page and take it off the DOM
 
-    // $(".button_to").submit(function(e){  //need to change the button_to to a form instead so that I can get the data from the user without having to go to new_user_deck_card_path(user, deck)
-    //
-    //   $.get(this.href).success(function(res){
-    //     ;
-    // });
-    //   // alert("target acquired");
-    //   e.preventDefault();
-    //   })
+//add deck id to each deck so that you can toggle just that one
+
+// make a small form to add the card to the deck
