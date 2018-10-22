a9425c98d38952f493ed0402f30e374899ef752c
funemployeddad <funemployeddad@gmail.com>
2018-10-21 17:35:59 -0700

    cleaned up repetitive code. working on targeting for specific deck to toggle.

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -1,13 +1,8 @@
 $(function(){
-  $(".button_to").hide();
   $(".table-cards").hide();
   $("a.load_deck").on('click', function(e){ //hijack the click event of selecting a deck
     e.preventDefault(); //prevent the move to the new page
-    $.get(this.href).success(function(res){ //get the href from the click event
-
-      $(".button_to").show(); //show the button. next step is to now add a form to the page for adding a card instead of a button that sends you to a new page.
-      $(".table-cards").toggle(); //need to get it to only toggle the list for that particular deckjosh
-      })
+      $(".table-cards").toggle(); //need to get it to only toggle the list for that particular deck
     });
   })
 
