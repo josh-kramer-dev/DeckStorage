b1373f1a74e7cc006530526bc80e8c1381763842
funemployeddad <funemployeddad@gmail.com>
2019-02-27 21:34:26 -0700

    target for adding a new deck via jQuery found. now need to figure out how to actually create the new object. look at handlebars and requirements for prototype to see if this is where it fits in'

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -3,6 +3,12 @@ $(function(){
     e.preventDefault();
     $(this).parent().find("div").toggleClass("cards");
   });
+
+  $( "form" ).submit(function (e) {
+  e.preventDefault();
+  $("div.decks").append("new deck");
+  //target works, now need to use jQuery to instatiate a new deck and figure out how to add cards to it.
+});
 })
 
 // make a small form to add the card to the deck
