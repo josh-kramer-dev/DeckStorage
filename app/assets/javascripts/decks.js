e0612f3f7e75af9275e73b350e9a3625e5dce1c4
funemployeddad <funemployeddad@gmail.com>
2019-02-27 20:48:30 -0700

    deck SHOW works; next is JS for deck NEW

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -1,23 +1,8 @@
 $(function(){
-  $(".show-deck").hide();
-  $("a.load_deck").on('click', function(e){ //hijack the click event of selecting a deck
-    e.preventDefault(); //prevent the move to the new page
-    var deck = document.getElementById(this.id)
-    console.log(deck)
-
-    $(".table-cards").toggle(deck); //need to get it to only toggle the list for that particular deck
+  $("a.load_deck").on('click', function(e) {
+    e.preventDefault();
+    $(this).parent().find("div").toggleClass("cards");
   });
 })
-
-    // $(".info").on('focus',function(){
-    //   $(".infoDiv").hide(); // will need to do this so that when you click on a different deck it closes out the one that was previously opened.
-    //   var id = $(this).attr('id');
-    //   $("#div"+id).show();
-    // });
-
-
-//hijack the card delete button and use it to remove the card but stay on the page and take it off the DOM
-
-//add deck id to each deck so that you can toggle just that one
 
 // make a small form to add the card to the deck
